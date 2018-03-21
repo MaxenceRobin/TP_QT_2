791f2f96f59c1681fadb51b8606b6bd8fc275ed8
MaxenceRobin <maxence.robin@etu.univ-tours.fr>
2018-03-21 15:14:32 +0100

    Ajout de la base auto gérée

--- TP_QT/dbmanager.cpp
+++ TP_QT/dbmanager.cpp
@@ -1,6 +1,8 @@
 #include "dbmanager.h"
 
-#include <QSqlDatabase>
+#include "selfmanageddatabase.h"
+
+#include <QSqlQuery>
 
 DBManager::DBManager()
 {
@@ -9,34 +11,49 @@ DBManager::DBManager()
 QSqlQueryModel * DBManager::getClientsModel()
 {
     // Connection to the database
-    QSqlDatabase dataBase = QSqlDatabase::addDatabase("QSQLITE");
+//    QSqlDatabase dataBase = QSqlDatabase::addDatabase("QSQLITE");
 
-    if (dataBase.isValid())
+//    if (dataBase.isValid())
+//    {
+//        dataBase.setHostName("localhost");
+//        dataBase.setUserName("root");
+//        dataBase.setPassword("password");
+
+////            if (QFile::exists("base_tmp.sqli"))
+////                QFile::remove("base_tmp.sqli");
+
+//        dataBase.setDatabaseName("base_tmp.sqli");
+//        dataBase.open();
+
+//        if (dataBase.isOpen())
+//        {
+//            QSqlQueryModel * model = new QSqlQueryModel;
+
+//            model->setQuery("select id, nom, prenom from TClient");
+//            model->setHeaderData(0, Qt::Horizontal, "Identifiant");
+//            model->setHeaderData(1, Qt::Horizontal, "Nom");
+//            model->setHeaderData(2, Qt::Horizontal, "Prénom");
+
+//            dataBase.close();
+//            dataBase.removeDatabase("QSQLITE");
+
+//            return model;
+//        }
+//    }
+
+    SelfManagedDatabase database;
+
+    if (database.isOpen())
     {
-        dataBase.setHostName("localhost");
-        dataBase.setUserName("root");
-        dataBase.setPassword("password");
+        QSqlQueryModel * model = new QSqlQueryModel;
 
-//            if (QFile::exists("base_tmp.sqli"))
-//                QFile::remove("base_tmp.sqli");
+        //model->setQuery("select id, nom, prenom from TClient");
+        model->setQuery(QSqlQuery("select id, nom, prenom from TClient"));
+        model->setHeaderData(0, Qt::Horizontal, "Identifiant");
+        model->setHeaderData(1, Qt::Horizontal, "Nom");
+        model->setHeaderData(2, Qt::Horizontal, "Prénom");
 
-        dataBase.setDatabaseName("base_tmp.sqli");
-        dataBase.open();
-
-        if (dataBase.isOpen())
-        {
-            QSqlQueryModel * model = new QSqlQueryModel;
-
-            model->setQuery("select id, nom, prenom from TClient");
-            model->setHeaderData(0, Qt::Horizontal, "Identifiant");
-            model->setHeaderData(1, Qt::Horizontal, "Nom");
-            model->setHeaderData(2, Qt::Horizontal, "Prénom");
-
-            dataBase.close();
-            dataBase.removeDatabase("QSQLITE");
-
-            return model;
-        }
+        return model;
     }
 
     return new QSqlQueryModel;
---
+++ TP_QT/selfmanageddatabase.cpp
@@ -0,0 +1,26 @@
+#include "selfmanageddatabase.h"
+
+SelfManagedDatabase::SelfManagedDatabase()
+    : QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE"))
+{
+    // Opening the database
+    if (isValid())
+    {
+        setHostName("localhost");
+        setUserName("root");
+        setPassword("password");
+
+        setDatabaseName("base_tmp.sqli");
+        open();
+    }
+}
+
+SelfManagedDatabase::~SelfManagedDatabase()
+{
+    // If the database was successfully opened, it has to be closed
+    if (isOpen())
+    {
+        close();
+        QSqlDatabase::removeDatabase("QSQLITE");
+    }
+}
